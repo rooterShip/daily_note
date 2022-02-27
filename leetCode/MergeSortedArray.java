2e790f8a4a9976fa41d7ad0fe04aa18fa99b59ba
nameLair-design <2486420157@qq.com>
2022-02-27 11:15:28 +0800

    合并两个有序数组

---
+++ leetCode/MergeSortedArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/*
+ * @Author: Rooter
+ * @Date: 2022-02-27 10:59:07
+ * @LastEditors: Rooter
+ * @LastEditTime: 2022-02-27 11:13:24
+ */
+/**
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。
+ * 为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。
+ * nums2 的长度为 n 。
+ * 
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+ * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+ */
+public class MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[]nums2, int n){
+        int cnt = 0;
+        for(int i = m; i < m+n; i++){
+            nums1[i] = nums2[cnt++];
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        merge(nums1, m, nums2, n);
+    }
+}
