6929ed95d4b69033284ec0d1dad71e9c927ad89a
nameLair-design <2486420157@qq.com>
2022-03-02 22:41:21 +0800

    对链表进行插入排序

---
+++ leetCode/InsertionListSort.java
@@ -0,0 +1,54 @@
+/*
+ * @Author: Rooter
+ * @Date: 2022-03-02 18:40:56
+ * @LastEditors: Rooter
+ * @LastEditTime: 2022-03-02 22:40:17
+ */
+/**
+ * 给定单个链表的头 head ，使用 插入排序 对链表进行排序，并返回 排序后链表的头 。
+ * 
+ * 输入: head = [4,2,1,3]
+ * 输出: [1,2,3,4]
+ */
+public class InsertionListSort {
+    public static ListNode insertionSortList(ListNode head){
+        if(head == null) return head;
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head;
+        ListNode curr = head.next;
+
+        while(curr != null){
+            if(curr.val >= lastSorted.val){
+                lastSorted = lastSorted.next;
+                curr = curr.next;
+            }
+            else{
+                ListNode prev = dummyHead;
+                while(prev.next.val <= curr.val){
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+                curr = lastSorted.next;
+            }
+        }
+        return dummyHead.next;  //此处必须为dummyHead.next不能为head，待解决
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val = val;}
+    ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
