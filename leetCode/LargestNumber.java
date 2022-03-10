d3ada7e4501f1c836c277d0af7466cc0770fa9a2
nameLair-design <2486420157@qq.com>
2022-03-10 22:24:28 +0800

    最大数

---
+++ leetCode/LargestNumber.java
@@ -0,0 +1,22 @@
+/*
+ * @Author: Rooter
+ * @Date: 2022-03-10 22:23:02
+ * @LastEditors: Rooter
+ * @LastEditTime: 2022-03-10 22:24:04
+ */
+/**
+ * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+ * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+ * 
+ * 输入：nums = [10,2]
+ * 输出："210"
+ * 
+ * 输入：nums = [3,30,34,5,9]
+ * 输出："9534330"
+ */
+public class LargestNumber {
+    public static void main(String[] args) {
+        
+    }
+    
+}
