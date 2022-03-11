e4ba33e7a3a337d9c0580991c3a5722ee1b26386
nameLair-design <2486420157@qq.com>
2022-03-11 23:20:12 +0800

    最大数

--- leetCode/LargestNumber.java
+++ leetCode/LargestNumber.java
@@ -2,7 +2,7 @@
  * @Author: Rooter
  * @Date: 2022-03-10 22:23:02
  * @LastEditors: Rooter
- * @LastEditTime: 2022-03-10 22:24:04
+ * @LastEditTime: 2022-03-11 23:18:58
  */
 /**
  * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
@@ -15,6 +15,10 @@
  * 输出："9534330"
  */
 public class LargestNumber {
+    public static String largestNumber(int[] nums) {
+        String ans = "hello world";
+        return ans;
+    }
     public static void main(String[] args) {
         
     }
