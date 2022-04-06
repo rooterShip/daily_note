497af934dc49c3991f3d58827162e337d5d58c78
nameLair-design <2486420157@qq.com>
2022-04-06 22:18:54 +0800

    最终改版

--- leetCode/LargestNumber2.java
+++ leetCode/LargestNumber2.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 /*
  * @Author: Rooter
  * @Date: 2022-04-01 22:26:19
  * @LastEditors: Rooter
- * @LastEditTime: 2022-04-05 22:30:53
+ * @LastEditTime: 2022-04-06 22:18:38
  */
 /**
  * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
@@ -13,26 +15,25 @@
  * 
  * 输入：nums = [3,30,34,5,9] 10,2,9,39,17
  * 输出："9534330"
+ * 
+ * 解答：https://leetcode-cn.com/problems/largest-number/solution/gong-shui-san-xie-noxiang-xin-ke-xue-xi-vn86e/
  */
 public class LargestNumber2 {
-    public static String largestNumber(int[] nums){
-        String res = "";
-        String[] temp = new String[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            temp[i] = String.valueOf(nums[i]);
-        }
-        if(temp.length==1) return temp[0];
-        for(int i = temp.length - 1; i > 0; i--){
-            if((temp[i] + temp[i-1]).compareTo(temp[i-1] + temp[i])>0){
-                String mid = temp[i];
-                temp[i] = temp[i-1];
-                temp[i-1] = mid;
-            }
-        }
-        for(int i = 0; i < temp.length; i++){
-            res = res + temp[i];
-        }
-        return res;
+    public static String largestNumber(int[] nums) {
+            int n = nums.length;
+            String[] ss = new String[n];
+            for (int i = 0; i < n; i++) ss[i] = "" + nums[i];
+            Arrays.sort(ss, (a, b) -> {
+                String sa = a + b, sb = b + a ;
+                return sb.compareTo(sa);
+            });
+            
+            StringBuilder sb = new StringBuilder();
+            for (String s : ss) sb.append(s);
+            int len = sb.length();
+            int k = 0;
+            while (k < len - 1 && sb.charAt(k) == '0') k++;
+            return sb.substring(k);
     }
     public static void main(String[] args) {
         int[] nums = {3,30,34,5,9};
