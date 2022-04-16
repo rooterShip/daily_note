58cbfebee8833003c6f15152f61346db1159c590
nameLair-design <2486420157@qq.com>
2022-04-16 21:12:10 +0800

    二分查找矩阵数字

--- leetCode/SearchMatrix.java
+++ leetCode/SearchMatrix.java
@@ -2,7 +2,7 @@
  * @Author: Rooter
  * @Date: 2022-04-14 22:16:41
  * @LastEditors: Rooter
- * @LastEditTime: 2022-04-14 23:13:06
+ * @LastEditTime: 2022-04-16 21:10:08
  */
 /**
  * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
@@ -12,31 +12,47 @@
  * 1 3 5 7
  * 10 11 16 20
  * 23 30 34 60
- * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3 
+ * {1,3,5,7},{10,11,16,20},{23,30,34,60}
  * 输出：true
+ * 
+ * [[1,3,5,7],[10,11,16,20],[23,30,34,60]] target = 13
  */
 public class SearchMatrix{
     public static boolean searchMatrix(int[][] matrix, int target){
-        int row[] = new int[matrix.length];
-        for(int i = 0; i < matrix.length; i++){
-            row[i] = matrix[i][0];
-        }
-        int lef = 0;
-        int reg = row.length - 1;
-        while(reg-lef!=1){
-            int mid = (lef + reg)/2;
-            if(target > row[mid]){
-                lef = mid;
+                int rowIndex = binarySearchFirstColumn(matrix, target);
+                if (rowIndex < 0) {
+                    return false;
+                }
+                return binarySearchRow(matrix[rowIndex], target);
+    }
+        
+    public static int binarySearchFirstColumn(int[][] matrix, int target) {
+            int low = -1, high = matrix.length - 1;
+            while (low < high) {
+                int mid = (high - low + 1) / 2 + low;
+                if (matrix[mid][0] <= target) {
+                    low = mid;
+                } else {
+                    high = mid - 1;
+                }
             }
-            else if(target < row[mid]){
-                reg = mid;
+            return low;
+    }
+        
+    public static boolean binarySearchRow(int[] row, int target) {
+        int low = 0, high = row.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (row[mid] == target) {
+                    return true;
+                } else if (row[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
-            else return true;
-        }
-        for(int i = 0; i < matrix[lef].length; i++){
-            if(target==matrix[lef][i]) return true;
-        }
-        return false;
+            return false;
     }
     public static void main(String[] args) {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
