7c1a56e001a7347df7de89d13a386e891181f28e
nameLair-design <2486420157@qq.com>
2022-05-04 21:43:35 +0800

    二叉树的前序遍历

--- leetCode/binaryTree/PreorderTraversal.java
+++ leetCode/binaryTree/PreorderTraversal.java
@@ -8,7 +8,7 @@ import java.util.Stack;
  * @Author: rooterShip
  * @Date: 2022-05-04 18:46:33
  * @LastEditors: rooterShip
- * @LastEditTime: 2022-05-04 19:01:20
+ * @LastEditTime: 2022-05-04 21:41:55
  */
 // 二叉树的前序遍历
 public class PreorderTraversal {
@@ -17,12 +17,12 @@ public class PreorderTraversal {
         Stack<TreeNode> stack = new Stack<>();
 
         while(root!=null || !stack.isEmpty()){
-            while(root!=null){
+            while(root!=null){  //尽可能的向左遍历 直到不能再向左，并将路径上的节点值加入res
                 res.add(root.val);
                 stack.push(root);
                 root = root.left;
             }
-            root = stack.pop();
+            root = stack.pop();//当不能再向左延伸的时候，弹出一个元素尝试向右稍微挪一下继续尝试向左
             root = root.right;
         }
         
