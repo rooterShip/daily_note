f6ce99f8c2cf125c57879b87dafda3523b931f03
nameLair-design <2486420157@qq.com>
2022-05-06 23:02:28 +0800

    二叉树的中序遍历（二叉树包下已经声明TreeNode类，现在注释掉，方便测试）

--- leetCode/binaryTree/InorderTraversal.java
+++ leetCode/binaryTree/InorderTraversal.java
@@ -8,7 +8,7 @@ import java.util.Stack;
  * @Author: rooterShip
  * @Date: 2022-04-30 22:31:46
  * @LastEditors: rooterShip
- * @LastEditTime: 2022-05-03 21:18:17
+ * @LastEditTime: 2022-05-06 23:00:37
  */
 /**
  * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
@@ -28,35 +28,36 @@ import java.util.Stack;
  * 前中后序遍历模板 https://www.bilibili.com/video/BV15K4y1Y7Gz?spm_id_from=333.999.0.0
  */
 public class InorderTraversal {
-    public static List<Integer> inorderTraversal(TreeNode root){
+    public static List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
 
-        while(root!=null || !stack.isEmpty()){
-            while(root!=null){  //尽可能的向左下方遍历，在路上把路径压入栈，找到不能再往左的时候就是最左，即结束循环
-                stack.add(root); //然后弹出栈中最后一个元素就是最左边的
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) { // 尽可能的向左下方遍历，在路上把路径压入栈，找到不能再往左的时候就是最左，即结束循环
+                stack.add(root); // 然后弹出栈中最后一个元素就是最左边的
                 root = root.left;
             }
             root = stack.pop();
-            res.add(root.val);  //左边走不通尝试稍微往右挪一下再继续往左走，期间在弹栈的过程中顺道把“根”位置的元素加入结果队列，实现 左 根 右
+            res.add(root.val); // 左边走不通尝试稍微往右挪一下再继续往左走，期间在弹栈的过程中顺道把“根”位置的元素加入结果队列，实现 左 根 右
             root = root.right;
         }
         return res;
     }
+
     public static void main(String[] args) {
-        
+
     }
 }
 
-class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+// class TreeNode{
+// int val;
+// TreeNode left;
+// TreeNode right;
+// TreeNode() {}
+// TreeNode(int val) { this.val = val; }
+// TreeNode(int val, TreeNode left, TreeNode right) {
+// this.val = val;
+// this.left = left;
+// this.right = right;
+// }
+// }
