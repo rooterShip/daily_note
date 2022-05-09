86baf1dd2ee435a693debfde9e01419c2867fb2d
nameLair-design <2486420157@qq.com>
2022-05-09 22:38:12 +0800

    N叉树的层序遍历（BFS、多叉树）

--- leetCode/LevelOrder.java
+++ leetCode/LevelOrder.java
@@ -6,7 +6,7 @@
  * 
  * @LastEditors: rooterShip
  * 
- * @LastEditTime: 2022-05-07 23:11:49
+ * @LastEditTime: 2022-05-09 22:36:53
  */
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -28,23 +28,23 @@ import java.util.Queue;
  * 输出：[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
  */
 public class LevelOrder {
-    public static List<List<Integer>> levelOrder(Node root) {
+    public static List<List<Integer>> levelOrder(MultiTreeNode root) {
         if (root == null) {
             return new ArrayList<List<Integer>>();
         }
 
         List<List<Integer>> ans = new ArrayList<>();
-        Queue<Node> queue = new ArrayDeque<>();
+        Queue<MultiTreeNode> queue = new ArrayDeque<>();
         queue.offer(root);
 
         while (!queue.isEmpty()) {
             int cnt = queue.size(); // 上一层的节点数
             List<Integer> level = new ArrayList<>();// 用来存放每一层的节点值
             for (int i = 0; i < cnt; i++) {
-                Node cur = queue.poll(); // 遍历该层的每一个节点
+                MultiTreeNode cur = queue.poll(); // 遍历该层的每一个节点
                 level.add(cur.val); // 每一层的节点值一个一个存进去
-                for (Node child : cur.children) {
-                    queue.offer(child); // 对该层的每一个节点的子节点放进队列，表示下一层的节点，进行下一层遍历
+                for (int j = 0; j < cur.children.size(); j++) {
+                    queue.offer(cur.children.get(j)); // 对该层的每一个节点的子节点放进队列，表示下一层的节点，进行下一层遍历
                 }
             }
             ans.add(level); // level代表每一层的节点，把不同层的放进去就是整个树
@@ -53,40 +53,52 @@ public class LevelOrder {
     }
 
     public static void main(String[] args) {
-        // TODO:补充测试用例
-        Node root = new Node(1);
-        Node A = new Node(3);
-        Node B = new Node(2);
-        Node C = new Node(4);
-        Node D = new Node(5);
-        Node E = new Node(6);
-        // FIXME:java.lang.NoSuchFieldError: children
-        List<Node> list1 = new ArrayList<>();
+        // 多叉树初始化要为每一个节点都设置相应的子节点才不会空指针报错
+        MultiTreeNode root = new MultiTreeNode(1);
+        MultiTreeNode A = new MultiTreeNode(3);
+        MultiTreeNode B = new MultiTreeNode(2);
+        MultiTreeNode C = new MultiTreeNode(4);
+        MultiTreeNode D = new MultiTreeNode(5);
+        MultiTreeNode E = new MultiTreeNode(6);
+
+        // root的children的初始化
+        List<MultiTreeNode> list1 = new ArrayList<>();
         list1.add(A);
         list1.add(B);
         list1.add(C);
-        List<Node> list2 = new ArrayList<>();
+        root.children = list1;
+        // A的children的初始化
+        List<MultiTreeNode> list2 = new ArrayList<>();
         list2.add(D);
         list2.add(E);
+        A.children = list2;
+        // B、C的children的初始化
+        List<MultiTreeNode> list3 = new ArrayList<>();
+        List<MultiTreeNode> list4 = new ArrayList<>();
+        B.children = list3;
+        C.children = list4;
+        // D、E的children的初始化
+        List<MultiTreeNode> list5 = new ArrayList<>();
+        List<MultiTreeNode> list6 = new ArrayList<>();
+        D.children = list5;
+        E.children = list6;
 
-        // root.children = new ArrayList<>(Arrays.asList(A, B, C));
-        // A.children = new ArrayList<>(Arrays.asList(D, E));
         System.out.println(levelOrder(root).toString());
     }
 }
-//FIXME:The type Node is already defined
-class Node {
-    public int val;
-    public List<Node> children;
 
-    public Node() {
+class MultiTreeNode {
+    public int val;
+    public List<MultiTreeNode> children;
+
+    public MultiTreeNode() {
     }
 
-    public Node(int _val) {
+    public MultiTreeNode(int _val) {
         val = _val;
     }
 
-    public Node(int _val, List<Node> _children) {
+    public MultiTreeNode(int _val, List<MultiTreeNode> _children) {
         val = _val;
         children = _children;
     }
