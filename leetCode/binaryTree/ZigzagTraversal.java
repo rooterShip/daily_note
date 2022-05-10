5cc7136b2e0af2298c6416231dc90d7ab3ea5f5b
nameLair-design <2486420157@qq.com>
2022-05-10 22:59:16 +0800

    二叉树的锯齿形层序遍历

---
+++ leetCode/binaryTree/ZigzagTraversal.java
@@ -0,0 +1,26 @@
+/*
+ * @Author: rooterShip
+ * @Date: 2022-05-10 22:56:50
+ * @LastEditors: rooterShip
+ * @LastEditTime: 2022-05-10 22:58:58
+ */
+/**
+ * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。
+ * （即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[20,9],[15,7]]
+ * 
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 
+ * 输入：root = []
+ * 输出：[]
+ */
+package binaryTree;
+
+public class ZigzagTraversal {
+    public static void main(String[] args) {
+
+    }
+}
