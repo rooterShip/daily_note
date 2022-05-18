7f28af55897981d69d9cb00894e113b2bd091248
nameLair-design <2486420157@qq.com>
2022-05-18 10:16:21 +0800

    257 二叉树所有路径

---
+++ leetCode/binaryTree/BinaryTreePaths.java
@@ -0,0 +1,35 @@
+
+/*
+ * @Author: rooterShip
+ * 
+ * @Date: 2022-05-18 10:12:58
+ * 
+ * @LastEditors: rooterShip
+ * 
+ * @LastEditTime: 2022-05-18 10:15:04
+ */
+/**
+ * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+ * 叶子节点 是指没有子节点的节点。
+ * 
+ * 输入：root = [1,2,3,null,5]
+ * 输出：["1->2->5","1->3"]
+ * 
+ * 输入：root = [1]
+ * 输出：["1"]
+ */
+package binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+    public static List<String> binaryTreePaths(TreeNode root){
+        List<String> res = new ArrayList<>();
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
