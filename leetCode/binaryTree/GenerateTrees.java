1e64b9e625098640d8b6dc559eeb0b3e4d197a48
rootership <2486420157@qq.com>
2022-06-08 21:47:37 +0800

    95. 不同的二叉搜索树 II

--- leetCode/binaryTree/GenerateTrees.java
+++ leetCode/binaryTree/GenerateTrees.java
@@ -6,7 +6,7 @@
  * 
  * @LastEditors: rooterShip
  * 
- * @LastEditTime: 2022-06-07 21:47:00
+ * @LastEditTime: 2022-06-08 21:45:59
  */
 /**
  * 95. 不同的二叉搜索树 II
@@ -18,6 +18,8 @@
 
  * 给你一个整数 n ，
  * 请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
+ * 
+ * 题解；https://leetcode.cn/problems/unique-binary-search-trees-ii/solution/cong-gou-jian-dan-ke-shu-dao-gou-jian-suo-you-shu-/
  */
 package binaryTree;
 
@@ -27,19 +29,32 @@ import java.util.List;
 public class GenerateTrees {
     public static List<TreeNode> generateTrees(int n) {
         List<TreeNode> res = new ArrayList<>();
-        return res;
-
+        if (n < 1)
+            return res;
+        return helper(1, n);
     }
 
-    public static TreeNode helper(int start, int end) {
-        if (start > end)
-            return null;
+    public static List<TreeNode> helper(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
 
-        int val = (start + end) / 2;// 折中为根节点（构造出的应该是平衡二叉树）
-        TreeNode root = new TreeNode(val);
-        root.left = helper(start, val - 1);
-        root.right = helper(val + 1, end);
-        return root;
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = helper(start, i - 1);
+            List<TreeNode> right = helper(i + 1, end);
+
+            for (TreeNode lef : left) {
+                for (TreeNode rig : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = lef;
+                    root.right = rig;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
     }
 
     public static void main(String[] args) {
