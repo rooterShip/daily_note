f44cff1a943d5922b515aaa08e83aa7bb2796dcb
rootership <2486420157@qq.com>
2022-06-09 22:18:49 +0800

    96. 不同的二叉搜索树

---
+++ leetCode/binaryTree/NumTrees.java
@@ -0,0 +1,25 @@
+
+/*
+ * @Author: rooterShip
+ * 
+ * @Date: 2022-06-09 22:16:34
+ * 
+ * @LastEditors: rooterShip
+ * 
+ * @LastEditTime: 2022-06-09 22:17:41
+ */
+/**
+ * 96. 不同的二叉搜索树
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ */
+package binaryTree;
+
+public class NumTrees {
+    public static int numTrees(int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
